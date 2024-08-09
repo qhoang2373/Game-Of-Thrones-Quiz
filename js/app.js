4f5fb0e5db14b9f2f77692b8de6383e6202fbf00
Quan <qhoang2373@gmail.com>
2024-08-08 18:46:41 -0700

    added event listeners

--- js/app.js
+++ js/app.js
@@ -9,3 +9,16 @@
 /*-------------------------------- Functions --------------------------------*/
 
 /*----------------------------- Event Listeners -----------------------------*/
+
+// - Add an event listener for the submit button to input answer
+submitButton.addEventListener('Submit')
+
+// - Add an event listener for a start button to start the game
+startButton.addEventListener('Start-Quiz')
+
+// - Add an event listener for a restart button to play again
+restartButton.addEventListener('Restart-Quiz', init)
+
+// - Add an event listener for a click sound to hear a sound when clicking
+clickSound.addEventListener('click', () => {
+    clickSound.play();
